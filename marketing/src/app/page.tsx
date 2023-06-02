df1243d6e0c6967d81b81c50d6f91fb12e3c3b55
utku şahin <utku@formsort.com>
2023-06-02 14:23:14 +0200

    feat: make home component async

--- marketing/src/app/page.tsx
+++ marketing/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { getLanding } from "@/api/queries/getLanding";
+import { getLanding, landingCollection } from "@/api/queries/getLanding";
 
-const Home = () => {
-  getLanding();
+const Home = async () => {
+  const { data, loading } = await getLanding();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      marketing 2
+      marketing 2 {JSON.stringify(data)}
     </main>
   );
 }
